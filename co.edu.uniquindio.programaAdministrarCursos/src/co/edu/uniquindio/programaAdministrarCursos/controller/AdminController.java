959f455bc0e959a2cb8218c1afbfce0152747037
optitudes <juans.rojasg@uqvirtual.edu.co>
2021-10-29 11:37:47 -0500

    creacion del mvc y terminacion del model de clases

---
+++ co.edu.uniquindio.programaAdministrarCursos/src/co/edu/uniquindio/programaAdministrarCursos/controller/AdminController.java
@@ -0,0 +1,16 @@
+package co.edu.uniquindio.programaAdministrarCursos.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class AdminController implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ co.edu.uniquindio.programaAdministrarCursos/src/co/edu/uniquindio/programaAdministrarCursos/controller/LoggingController.java
@@ -0,0 +1,16 @@
+package co.edu.uniquindio.programaAdministrarCursos.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class LoggingController implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
